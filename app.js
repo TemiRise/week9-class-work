d16e0fe408b61f7ba9168306f4f3cc4077b42a60
Orieke Misan Clement <temiswift@gmail.com>
2021-07-27 11:51:09 +0100

    week9classstuff

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 7000
+const userRoute = require('./route/UserRoute'); //to import the user route
+app.use(express.json()); //middleware
+app.use('/api/user', userRoute) //middleware to define the user router
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my API!')
+})
+// mongodb+srv://TemiRise:<Oyomi2021>@cluster0.bouk2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+app.listen(port, () => {
+  console.log(`Example app listening at http:127.0.0.1:7000`)
+})
